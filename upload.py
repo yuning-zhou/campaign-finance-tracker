554d529de5c709098beda43a215e60e57ef06fcc
Yuning Zhou <yuning.zhou@stern.nyu.edu>
2020-06-04 20:37:30 -0400

    implemented auto uploading to s3

--- upload.py
+++ upload.py
@@ -1,31 +1,4 @@
-import logging
 import boto3
-from botocore.exceptions import ClientError
 
-
-def upload_file(file_name, bucket, object_name=None):
-    """
-    Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = file_name
-
-    # Upload the file
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-
-s3 = boto3.client('s3')
-with open("FILE_NAME", "rb") as f:
-    s3.upload_fileobj(f, "BUCKET_NAME", "OBJECT_NAME")
+s3 = boto3.resource('s3')
+s3.Object('campaign-finance-tracker', 'Candidate-18.txt').put(Body=open('./Candidate18.txt', 'rb'))

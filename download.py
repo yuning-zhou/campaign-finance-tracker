ce1b8af23204951cedb711fc641e709c846ded1b
Yuning Zhou <yuning.zhou@stern.nyu.edu>
2020-06-04 20:18:50 -0400

    implemented downloading, extracting and renaming

--- download.py
+++ download.py
@@ -1,6 +1,14 @@
 import requests
+import zipfile
+import os
 
-url = 'https://www.fec.gov/files/bulk-downloads/2020/weball20.zip'
+
+url = 'https://www.fec.gov/files/bulk-downloads/2018/weball18.zip'
 r = requests.get(url, allow_redirects=True)
 
-open('test1.zip', 'wb').write(r.content)
+open('Candidate18.zip', 'wb').write(r.content)
+
+with zipfile.ZipFile('Candidate18.zip', 'r') as zip_ref:
+    zip_ref.extractall()
+
+os.rename('weball18.txt', 'Candidate18.txt')
